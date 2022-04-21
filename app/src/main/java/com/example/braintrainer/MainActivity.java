9c841c906ea46607052e225cfc20596f782dd800
GandiYahya <gandiyahya42@gmail.com>
2022-04-21 09:33:46 +0700

    menambahkan komen

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    // ini perubahan baru
     Button startButton;
     TextView resultTextView;
     TextView pointTextView;
